c7c92a52019fcd21e099d05ea479c04407a1ac47
Hrannar Hliddal Thorvaldsson <hht4@hi.is>
2021-03-18 15:34:49 +0000

    Update

--- app/src/main/java/is/hi/hbv601g/hottopicsquiz/MainActivity.java
+++ app/src/main/java/is/hi/hbv601g/hottopicsquiz/MainActivity.java
@@ -1,8 +1,11 @@
 package is.hi.hbv601g.hottopicsquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +17,7 @@ import static is.hi.hbv601g.hottopicsquiz.R.string.incorrect_toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final String KEY_INDEX = "index";
     private Button mButtonTrue;
     private Button mButtonFalse;
     private Button mButtonNext;
@@ -29,7 +33,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_main);
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
+
         updateQuestions();
 
         mButtonTrue =(Button) findViewById(R.id.True_Button);
@@ -56,6 +65,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    public void onSaveInstanceState( Bundle SavedInstanceState) {
+        super.onSaveInstanceState(SavedInstanceState);
+        SavedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+
     //Updates question text to current question
     private void updateQuestions(){
         mTextViewQuestion = (TextView) findViewById(R.id.question_text);
