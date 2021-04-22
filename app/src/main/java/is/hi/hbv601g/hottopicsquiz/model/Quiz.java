6eef51ec35939b8bab2cd6784c32703e84e24f73
Hrannar Hliddal Thorvaldsson <hht4@hi.is>
2021-04-22 10:35:31 +0000

    Update Quiz.java

--- app/src/main/java/is/hi/hbv601g/hottopicsquiz/model/Quiz.java
+++ app/src/main/java/is/hi/hbv601g/hottopicsquiz/model/Quiz.java
@@ -7,7 +7,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-public class Quiz implements Parcelable {
+public class
+Quiz implements Parcelable {
 
     private Long id;
     private List<Question> questions;
